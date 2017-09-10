af5a5db4a6a072e95c6e67c97df634b641b3c347
vstrimaitis <vstrimaitis@gmail.com>
2017-09-10 18:59:46 +0300

    Added TaskRenderer strategy implementations.

---
+++ Strategy/Core/HtmlTaskRenderer.cs
@@ -0,0 +1,15 @@
+﻿namespace Strategy.Core
+{
+    class HtmlTaskRenderer : ITaskRenderer
+    {
+        public string RenderTaskBody(Task t)
+        {
+            return $"<p>Maximum score: <b>{t.MaxScore}</b></p>\n<p>{t.Description}</p>";
+        }
+
+        public string RenderTaskHeader(Task t)
+        {
+            return $"<h1>{t.Name}</h1>";
+        }
+    }
+}
---
+++ Strategy/Core/MarkdownTaskRenderer.cs
@@ -0,0 +1,15 @@
+﻿namespace Strategy.Core
+{
+    class MarkdownTaskRenderer : ITaskRenderer
+    {
+        public string RenderTaskBody(Task t)
+        {
+            return $"> Maximum score: {t.MaxScore} points\n> {t.Description}";
+        }
+
+        public string RenderTaskHeader(Task t)
+        {
+            return $"# {t.Name}";
+        }
+    }
+}
